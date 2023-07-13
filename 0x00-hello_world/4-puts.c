1ab4d611d52921cd8b19196106afd772972e76e6
yousefdev19 <yousefdev19@gmail.com>
2023-07-13 13:33:53 -0700

    repair

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 int main(void)
 {
 	/*
-	 * hello this is first function
+	 *main:  entry point
+	 *return:  always 0 (success)
 	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
