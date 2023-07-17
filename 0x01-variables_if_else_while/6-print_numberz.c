508aa3e8efa30108a7f479e6d427f79133b5f53b
yousefdev19 <yousefdev19@gmail.com>
2023-07-17 11:10:21 -0700

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program print text
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
