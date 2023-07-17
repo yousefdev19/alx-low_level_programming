c7c11df6583d37546d68481a0560aa36ff3f4ce9
yousefdev19 <yousefdev19@gmail.com>
2023-07-17 12:36:18 -0700

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program print text
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
