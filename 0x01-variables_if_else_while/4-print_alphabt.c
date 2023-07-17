8e34a9a6ddba674218974497faba26d8670fee80
yousefdev19 <yousefdev19@gmail.com>
2023-07-17 10:43:33 -0700

    4 print alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program print text
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	for (int ch = 97; ch <= 122; ch++)
+{
+	if (ch == 101 || ch == 113)
+	{
+		ch++;
+		continue;
+	}
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+
+}
