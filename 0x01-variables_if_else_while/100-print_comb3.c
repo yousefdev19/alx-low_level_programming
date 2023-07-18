e81f06dbc1f389c8a76c649be1c864191cf6396c
yousefdev19 <yousefdev19@gmail.com>
2023-07-18 09:56:09 -0700

    nested loops and function

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program print text
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program print text
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
