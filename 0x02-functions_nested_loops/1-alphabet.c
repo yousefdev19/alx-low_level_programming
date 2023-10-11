d2a4461c0e8dc32839c8518871ded4f229a7ec61
yousefdev19 <yousefdev19@gmail.com>
2023-10-11 09:56:29 -0700

    nwe

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: this program print alphapet
+ * parameter: their is no parameter
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+   putchar(10);
+   return(0);
+}
